9a56f39bc266824862c64d65333a96c8b3184d13
Adam Drewery <8437373+adam-drewery@users.noreply.github.com>
2025-03-31 01:04:13 +0200

    Update item icons based on rarity levels
    Added conditional logic to assign specific SVG icons corresponding to item rarity levels. Ensured a fallback icon is used for unrecognized rarities, improving the visual differentiation of items.

--- src/control/materials_list.rs
+++ src/control/materials_list.rs
@@ -1,5 +1,5 @@
 use crate::event::{Event, Material};
-use crate::image::GRADE_1;
+use crate::image::{COURIER_ICON, GRADE_1, GRADE_2, GRADE_3, GRADE_4, GRADE_5};
 use crate::theme::{GRAY, ORANGE, YELLOW};
 use iced::widget::svg::Handle;
 use iced::widget::{column, row, scrollable, svg, text, Column};
@@ -11,11 +11,22 @@ pub fn materials_list(title: &str, items: Vec<Material>) -> Column<Event> {
         scrollable(column(
             order_items(items)
                 .map(|item| {
+                    
+                    let svg_handle = match item.info().rarity.as_str() {
+                        "Very Common" => Handle::from_memory(GRADE_1),
+                        "Common" => Handle::from_memory(GRADE_2),
+                        "Standard" => Handle::from_memory(GRADE_3),
+                        "Rare" => Handle::from_memory(GRADE_4),
+                        "Very Rare" => Handle::from_memory(GRADE_5),
+                        _ => Handle::from_memory(COURIER_ICON),
+                    };
+
                     row![
-                        column![svg(Handle::from_memory(GRADE_1)).height(16).width(16)].padding([0, 5]),
+                        column![svg(svg_handle).height(16).width(16)].padding([0, 5]),
                         text(item.count).size(16).color(YELLOW).width(36),
                         text(item.display_name()).size(16),
-                        column![text(item.info().category.clone()).size(16).color(GRAY)].padding([0, 5]),
+                        column![text(item.info().category.clone()).size(16).color(GRAY)]
+                            .padding([0, 5]),
                     ]
                     .padding(2)
                 })
