3d17ab4f85f4702b4c3b2cdb7f950a58cf6a9c4e
Adam Drewery <8437373+adam-drewery@users.noreply.github.com>
2025-04-03 00:20:45 +0200

    Move `Powerplay` event to personal module.

--- src/event/personal/powerplay.rs
+++ src/event/personal/powerplay.rs
@@ -1,6 +1,24 @@
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug, Default, Clone)]
+pub struct Powerplay {
+
+    pub timestamp: String,
+
+    #[serde(rename = "Power")]
+    pub power: String,
+
+    #[serde(rename = "Rank")]
+    pub rank: u8,
+
+    #[serde(rename = "Merits")]
+    pub merits: u32,
+
+    #[serde(rename = "TimePledged")]
+    pub time_pledged: u64
+}
+
+#[derive(Deserialize, Debug, Default, Clone)]
 pub struct PowerplayJoin {
 
     pub timestamp: String,
