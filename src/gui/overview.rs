3e453fbd0f840616c1c3ed831de2a874006b8226
Adam Drewery <8437373+adam-drewery@users.noreply.github.com>
2025-07-28 21:33:19 +0200

    Refactor overview GUI: modularize `location`, `route`, and `personal` sections into separate modules for improved code organization and maintainability.

--- src/gui/overview.rs
+++ src/gui/overview.rs
@@ -1,18 +1,16 @@
 mod ship;
+mod location;
+mod personal;
 
 use crate::event::JournalEvent;
-use crate::gui::components::{details, details_extra, header, sub_header};
-use crate::gui::overview::ship::ship;
-use crate::image::FUEL_STAR_PNG;
+use crate::gui::components::header;
+use crate::gui::overview::location::*;
+use crate::gui::overview::personal::*;
+use crate::gui::overview::ship::*;
 use crate::state::State;
-use crate::theme::{DARK_GRAY, GRAY, ORANGE, RED, WHITE};
-use iced::border::radius;
-use iced::widget::image::Handle;
-use iced::widget::{column, container, image, row, scrollable, text, Column, Row};
-use iced::{Border, Element, Fill, Theme};
-use std::f64;
-use thousands::Separable;
-use crate::fonts::eurocaps::FONT;
+use crate::theme::{GRAY, ORANGE, WHITE};
+use iced::widget::{column, row, scrollable, text, Column, Row};
+use iced::{Element, Fill};
 
 pub fn overview(state: &State) -> Row<JournalEvent> {
     row![
@@ -69,147 +67,4 @@ fn messages(state: &State) -> Column<JournalEvent> {
             .anchor_bottom()
     ]
     .height(256)
-}
-
-fn location(state: &State) -> Column<JournalEvent> {
-    let mut result = column![
-        header("Location"),
-        
-        sub_header("System"),
-        details("Government", &state.location.system_government),
-        details("Economy", &state.location.system_economy),
-        details("Population", state.location.population.to_string().separate_with_commas()),
-        details("Security", &state.location.system_security.replace(" Security", "")),
-        details("Allegiance", &state.location.system_allegiance),
-    ]
-        .padding(8);
-
-    if state.location.powerplay_state.is_some() {
-        result = result.push(sub_header("Powerplay"))
-            .push(details_extra(
-                "Controller", 
-                state.location.controlling_power.clone().unwrap_or_default(),
-                state.location.powerplay_state_control_progress.map(|x| format!("{:.2}%", x * 100.0)).unwrap_or_default()))
-            .push(details("State", state.location.powerplay_state.clone().unwrap_or_default()))
-            .push(details("Reinforcement", state.location.powerplay_state_reinforcement.map(|x| x.to_string()).unwrap_or_default()))
-            .push(details("Undermining", state.location.powerplay_state_undermining.map(|x| x.to_string()).unwrap_or_default()));
-    }
-
-    if state.location.factions.is_some() {
-        result = result.push(sub_header("Factions"));
-
-        // todo: why do i have to clone this, i don't wanna
-        for faction in state.location.factions.clone().unwrap() {
-            let states = if let Some(active_states) = &faction.active_states {
-                let state_names: Vec<String> = active_states.iter().map(|s| s.state.clone()).collect();
-                format!("{} | {} | {}", faction.allegiance, faction.government, state_names.join(" | "))
-            } else {
-                format!("{} | {}", faction.allegiance, faction.government)
-            };
-
-            result = result.push(
-                row![
-                    column![text(faction.name)],
-                    column![].width(12),
-                    column![text(states).color(GRAY)],
-                    column![].width(Fill),
-                    column![text(format!("{:.2}%", faction.my_reputation)).color(GRAY)],
-                    column![].width(12),
-                    column![text(format!("{:.2}%", faction.influence * 100.0)).color(GRAY)],
-                ]
-            );
-        }
-    }
-
-    result
-}
-
-fn personal(state: &State) -> Column<JournalEvent> {
-    column![
-        header("Personal"),
-        details("Suit Name", &state.suit_loadout.suit_name_localised),
-        details("Loadout", &state.suit_loadout.loadout_name),
-        details("Empire", state.reputation.empire.to_string()),
-        details("Federation", state.reputation.federation.to_string()),
-        details("Alliance", state.reputation.alliance.to_string()),
-    ]
-    .padding(8)
-}
-
-fn route(state: &State) -> Column<JournalEvent> {
-    let title_column = column![header("Route")].padding(8);
-
-    if state.nav_route.len() == 0 {
-        return column![
-            title_column,
-            row![].height(Fill),
-            row![
-                column![].width(Fill),
-                column![text("No current route").font(FONT)],
-                column![].width(Fill),
-            ],
-            row![].height(Fill),
-        ]
-    }
-
-    let mut items_column = column![].padding(8);
-
-
-
-    for i in 0..state.nav_route.len() {
-        let route_step = &state.nav_route[i];
-        if i != 0 {
-            let prev_step = &state.nav_route[i - 1];
-            let distance = calculate_distance(&prev_step.star_pos, &route_step.star_pos);
-
-            let mut icons_column = column![];
-            let mut star_type_text = text(&route_step.star_class);
-
-            if route_step.is_fuel_star() {
-                icons_column = icons_column.push(row![image(Handle::from_bytes(FUEL_STAR_PNG)).width(12).height(12)].padding(3));
-            }
-            else {
-                star_type_text = star_type_text.color(RED);
-            }
-
-            items_column = items_column.push(
-                row![
-                    container(row![
-                        column![text(&route_step.star_system)],
-                        column![].width(Fill),
-                        column![star_type_text],
-                        icons_column,
-                        column![].width(16),
-                        column![text(format!("{:.2} ly", distance))]
-                    ])
-                    .style(route_step_style)
-                    .padding(8)
-                ]
-                .padding(8)
-                .width(Fill),
-            );
-        }
-    }
-
-    column![title_column, scrollable(items_column)].height(Fill)
-}
-
-fn calculate_distance(pos1: &Vec<f64>, pos2: &Vec<f64>) -> f64 {
-    let dx = pos2[0] - pos1[0];
-    let dy = pos2[1] - pos1[1];
-    let dz = pos2[2] - pos1[2];
-    f64::sqrt(dx * dx + dy * dy + dz * dz)
-}
-
-fn route_step_style(_theme: &Theme) -> container::Style {
-    container::Style {
-        background: Some(DARK_GRAY.into()),
-        text_color: Some(WHITE),
-        border: Border {
-            width: 0.0,
-            color: ORANGE,
-            radius: radius(0),
-        },
-        shadow: Default::default(),
-    }
 }
---
+++ src/gui/overview/personal.rs
@@ -0,0 +1,17 @@
+use crate::event::JournalEvent;
+use crate::gui::components::{details, header};
+use crate::state::State;
+use iced::widget::Column;
+
+pub fn personal(state: &State) -> Column<JournalEvent> {
+
+    iced::widget::column![
+        header("Personal"),
+        details("Suit Name", &state.suit_loadout.suit_name_localised),
+        details("Loadout", &state.suit_loadout.loadout_name),
+        details("Empire", state.reputation.empire.to_string()),
+        details("Federation", state.reputation.federation.to_string()),
+        details("Alliance", state.reputation.alliance.to_string()),
+    ]
+    .padding(8)
+}
